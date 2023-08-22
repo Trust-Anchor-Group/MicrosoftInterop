2fbe4b275e8cfb3ff000bb31193ef42d00641356
Peter Waher <peterwaher@hotmail.com>
2023-08-22 18:50:29 -0400

    Conversion service

---
+++ TAG.Service.MicrosoftInterop/WebServices/WordToMarkdown.cs
@@ -0,0 +1,75 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using System.Text;
+using System.Threading.Tasks;
+using TAG.Content.Microsoft;
+using Waher.Content.Markdown;
+using Waher.Networking.HTTP;
+
+namespace TAG.Service.MicrosoftInterop.WebServices
+{
+	/// <summary>
+	/// Converts a Word document to Markdown
+	/// </summary>
+	public class WordToMarkdown : HttpSynchronousResource, IHttpPostMethod
+	{
+		private readonly HttpAuthenticationScheme[] authenticationSchemes;
+
+		/// <summary>
+		/// Converts a Word document to Markdown
+		/// </summary>
+		/// <param name="AuthenticationSchemes">Authentication schemes.</param>
+		public WordToMarkdown(params HttpAuthenticationScheme[] AuthenticationSchemes)
+			: base("/WordToMarkdown")
+		{
+			this.authenticationSchemes = AuthenticationSchemes;
+		}
+
+		/// <summary>
+		/// If sub-paths are handled.
+		/// </summary>
+		public override bool HandlesSubPaths => false;
+
+		/// <summary>
+		/// If User sessions are required
+		/// </summary>
+		public override bool UserSessions => true;
+
+		/// <summary>
+		/// Gets available authentication schemes
+		/// </summary>
+		/// <param name="Request">Request object.</param>
+		/// <returns>Array of authentication schemes.</returns>
+		public override HttpAuthenticationScheme[] GetAuthenticationSchemes(HttpRequest Request)
+		{
+			return this.authenticationSchemes;
+		}
+
+		/// <summary>
+		/// If the POST method is supported.
+		/// </summary>
+		public bool AllowsPOST => true;
+
+		/// <summary>
+		/// Executes the POST method
+		/// </summary>
+		/// <param name="Request">Request object.</param>
+		/// <param name="Response">Response object.</param>
+		public async Task POST(HttpRequest Request, HttpResponse Response)
+		{
+			if (!Request.HasData)
+				throw new BadRequestException("No content.");
+
+			object Decoded = await Request.DecodeDataAsync();
+			if (!(Decoded is WordprocessingDocument Doc))
+				throw new BadRequestException("Content not a Word document (.docx).");
+
+			StringBuilder Markdown = new StringBuilder();
+			WordUtilities.ExtractAsMarkdown(Doc, string.Empty, Markdown);
+
+			byte[] Data = WordToMarkdownConverter.Utf8WithBOM.GetBytes(Markdown.ToString());
+
+			Response.ContentType = MarkdownCodec.ContentType + "; charset=utf-8";
+			await Response.Write(Data);
+		}
+	}
+}
